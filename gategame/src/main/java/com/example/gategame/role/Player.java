d57bf702e35c28699384082cca029b829ea7fa01
Hao Ye <u7981083@anu.edu.au>
2024-09-10 20:22:46 +1000

    Init a new game project
    Use gradle for dependency management

---
+++ gategame/src/main/java/com/example/gategame/role/Player.java
@@ -0,0 +1,15 @@
+package com.example.gategame.role;
+
+/**
+ * @author Hao Ye(u7981083)
+ */
+public class Player extends Role {
+
+
+    public Player(String name, String description, Integer power, Integer maxHealth) {
+        super(name, description, power, maxHealth);
+    }
+
+
+
+}
