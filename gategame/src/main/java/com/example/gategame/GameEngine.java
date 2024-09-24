60d6d99399be8c6a298fc4cd97ca69802973718c
Hao Ye <u7981083@anu.edu.au>
2024-09-24 20:14:52 +1000

    create monster
    Closes #24

--- gategame/src/main/java/com/example/gategame/GameEngine.java
+++ gategame/src/main/java/com/example/gategame/GameEngine.java
@@ -2,16 +2,38 @@ package com.example.gategame;
 
 import com.example.gategame.map.MapFactory;
 import com.example.gategame.settings.GameConfigLoader;
+import com.example.gategame.settings.MonstersConfig;
+import com.example.gategame.settings.SettingsConfig;
 
 /**
  * @author Hao Ye(u7981083)
+ * Construct the game from config
  */
 public class GameEngine {
+
+    private static final GameEngine instance = new GameEngine();
+
+    private GameEngine() {
+    }
+
+    public static GameEngine getInstance() {
+        return instance;
+    }
+
+    private SettingsConfig settingsConfig;
+
+    public SettingsConfig getSettingsConfig() {
+        return settingsConfig;
+    }
+
+    public MonstersConfig getMonsterConfig() {
+        return settingsConfig.getMonstersConfig();
+    }
 
     public static Integer level;
 
     public static void main(String[] args) {
-        GameConfigLoader.loadConfig();
+        GameEngine.getInstance().settingsConfig = GameConfigLoader.loadConfig();
         // start play
         setLevel(1);
     }
---
+++ gategame/src/main/java/com/example/gategame/role/MinorMonster.java
@@ -0,0 +1,17 @@
+package com.example.gategame.role;
+
+import com.example.gategame.backpack.Backpack;
+
+/**
+ * @author Hao Ye(u7981083)
+ */
+public class MinorMonster extends Monster {
+    public MinorMonster(Integer power, Integer maxHealth, Backpack backpack) {
+        super("MinorMonster", "", power, maxHealth, backpack);
+    }
+
+    @Override
+    public Character getSymbol() {
+        return 'M';
+    }
+}
